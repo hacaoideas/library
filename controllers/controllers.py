b10b40b62bdb04a91174c4ce85b120cd4b47ad4c
hacaoideas <hacaoideas@gmail.com>
2020-06-07 11:52:05 +0700

    starting json

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+import odoo
+import json
 
 class Books(http.Controller):
 
@@ -12,8 +14,31 @@ class Books(http.Controller):
         )
 
     @http.route('/partners', auth='user')
-    def list(self):
+    def list_2(self):
         Partners = http.request.env['res.partner'].search([])
         return http.request.render(
             'library.partner_list_template', {'partners':Partners}
         )
+
+class PetHandler(http.Controller):
+
+    @http.route('/json_test/<dbname>', auth='public', type='json', methods=['GET'], sitemap=False, cors='*', csrf=False)
+    def list_3(self, dbname):
+        model_name = 'res.partner'
+        try:
+            registry = odoo.modules.registry.Registry(dbname)
+            with odoo.api.Environment.manage(), registry.cursor() as cr:
+                env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
+                rec = env[model_name].search([], limit=1)
+                response = {
+                    "status": "ok",
+                    "content": {
+                        "name": rec.name,
+                    }
+                }
+        except Exception:
+            response = {
+                "status": "error",
+                "content": "not found",
+            }
+        return json.dumps(response)
